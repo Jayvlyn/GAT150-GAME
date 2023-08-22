657165cfc1a2bd89d10a933de061e0d96f1672af
Jacob Leonardo <leonardojacob257@gmail.com>
2023-08-22 11:48:07 -0600

    Text Component added

--- Source/Engine/Renderer/Text.cpp
+++ Source/Engine/Renderer/Text.cpp
@@ -19,6 +19,7 @@ namespace kiko
 		m_texture = SDL_CreateTextureFromSurface(renderer.m_renderer, surface);
 		SDL_FreeSurface(surface);
 	}
+
 	void Text::Draw(Renderer& renderer, int x, int y)
 	{
 		int width, height;
@@ -26,5 +27,21 @@ namespace kiko
 
 		SDL_Rect rect{ x, y, width, height };
 		SDL_RenderCopy(renderer.m_renderer, m_texture, NULL, &rect);
+	}
+
+	void Text::Draw(Renderer& renderer, const Transform& transform)
+	{
+		int width, height;
+		SDL_QueryTexture(m_texture, nullptr, nullptr, &width, &height);
+		mat3 mx = transform.GetMatrix();
+		vec2 position = mx.GetTranslation();
+		vec2 size = vec2{ width, height } *mx.GetScale();
+		SDL_Rect dest;
+		dest.x = (int)(position.x - (size.x * 0.5f));
+		dest.y = (int)(position.y - (size.y * 0.5f));
+		dest.w = (int) size.x;
+		dest.h = (int) size.y;
+		// https://wiki.libsdl.org/SDL2/SDL_RenderCopyEx
+		SDL_RenderCopyEx(renderer.m_renderer, m_texture, nullptr, &dest, kiko::RadToDeg(mx.GetRotation()), nullptr, SDL_FLIP_NONE);
 	}
 }
